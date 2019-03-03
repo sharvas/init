3576722f0e964bd5e7bca3e88debb54a0c3e4033
Sarunas Vaskelis <40366498+sharvas@users.noreply.github.com>
2019-03-03 22:12:17 +0100

    Update vm_script.sh

--- vm_script.sh
+++ vm_script.sh
@@ -1,27 +1,16 @@
 mkdir -p /Volumes/Storage/goinfre/$USER/vagrant
-
 echo 'export VAGRANT_HOME=/Volumes/Storage/goinfre//vagrant' >> ~/.zshrccd /Volumes/Storage/goinfre/$USER/vagrant
-
 cat > Vagrantfile << EOM
 
 Vagrant.configure("2") do |config|
-
 	config.vm.box = "debian/stretch64"
-
 	config.vm.synced_folder ".", "/git"
-
 end
 
 EOM
-
 vagrant up
-
 vagrant plugin install vagrant-vbguest
-
 vagrant vb-guest
-
 vagrant reload
-
 cd /Volumes/Storage/goinfre/$USER/vagrant
-
 vagrant ssh
